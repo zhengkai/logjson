5ec2e4f5f2516b37cdfa262cdf8cf68d8cd35ca8
Zheng Kai <zhengkai@gmail.com>
2019-01-11 17:47:00 +0800

    up

--- log.php
+++ log.php
@@ -1,10 +1,16 @@
 <?php
 if ($_POST || !$_POST['info'] || !is_string($_POST['info'])) {
-	exit;
+	// exit;
 }
 
 file_put_contents(
 	__DIR__ . '/log/' . date('m-d-H') . '.txt',
 	$_POST['info'],
+	LOCK_EX | FILE_APPEND
+);
+
+file_put_contents(
+	__DIR__ . '/log/' . date('m-d-H') . '.txt',
+	file_get_contents("php://input"),
 	LOCK_EX | FILE_APPEND
 );
