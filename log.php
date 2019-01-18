869fed691e5a865e1043d2f9429eb5707803c502
Zheng Kai <zhengkai@gmail.com>
2019-01-18 18:00:43 +0800

    up

--- log.php
+++ log.php
@@ -1,16 +1,27 @@
 <?php
-if ($_POST || !$_POST['info'] || !is_string($_POST['info'])) {
-	// exit;
+if (!$_POST || empty($_POST['v']) || !is_string($_POST['v'])) {
+	exit;
 }
 
+$s =& $_POST['type'];
+if (!is_string($s) || !preg_match('#^[A-Za-z0-9]{1,10}$#', $s)) {
+	exit;
+}
+
+define('NOW', $_SERVER['REQUEST_TIME']);
+
+$filename = $s . date('-Ymd', NOW) . '.txt';
+
 file_put_contents(
-	__DIR__ . '/log/' . date('m-d-H') . '.txt',
-	'post ' . json_encode($_POST) . "\n",
+	__DIR__ . '/log/' . $filename,
+	date('Y-m-d H:i:s', NOW) . "\n" . $_POST['v'] . "\n\n",
 	LOCK_EX | FILE_APPEND
 );
 
+/*
 file_put_contents(
-	__DIR__ . '/log/' . date('m-d-H') . '.txt',
+	__DIR__ . '/log/' . $filename,
 	'json ' . file_get_contents("php://input") . "\n",
 	LOCK_EX | FILE_APPEND
 );
+ */
